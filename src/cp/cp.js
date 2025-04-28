d962b89d0580b5e37a685991b466ca9f1072bc0d
ViktoriaRosovska <torimeister@gmail.com>
2025-04-28 20:09:41 +0300

    feat: add child process cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,31 @@
+import {fileURLToPath} from "url";
+import { spawn } from "child_process";
+import path from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const sourceDir = "files";
+    const sourceFile = "script.js";
+
+    const filePath = path.join(__dirname, sourceDir, sourceFile);
+
+    const childFile = spawn('node', [filePath, ...args]);
+
+    process.stdin.pipe(childFile.stdin);
+
+    childFile.stdout.pipe(process.stdout)
+
+    childFile.on('error', (err) => {
+        console.log("Error with start children process: ", err);
+    })
+
+    childFile.on('exit', (code) => {
+        console.log(`Child precess finished with code ${code}`);
+    })
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ["someArgument1", "someArgument2", "RSS"]);
